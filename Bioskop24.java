73305017b5af9ab80c8d9c3f52df0cd2acc5ae04
Refaliya-Vinandha <revaliya80@gmail.com>
2025-11-12 21:46:28 +0700

    modifikasi 1 dan 2

--- Bioskop24.java
+++ Bioskop24.java
@@ -19,7 +19,7 @@ public class Bioskop24 {
         System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
         System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
 
-        System.err.println(penonton.length);
+        System.out.println(penonton.length);
         System.out.println(penonton[0].length);
         System.out.println(penonton[1].length);
         System.out.println(penonton[2].length);

67cbd44c37c6a40a362466a0d64d41dcc3948321
Refaliya-Vinandha <revaliya80@gmail.com>
2025-11-12 16:43:05 +0700

    Percobaan 1

--- Bioskop24.java
+++ Bioskop24.java
@@ -13,9 +13,9 @@ public class Bioskop24 {
         penonton[2][1] = "Farhan";
         penonton[3][0] = "Gisel";
 
-        System.out.printf("%s\t%s\n", penonton[0][0], penonton[0][1]);
-        System.out.printf("%s\t%s\n", penonton[1][0], penonton[1][1]);
-        System.out.printf("%s\t%s\n", penonton[2][0], penonton[2][1]);
-        System.out.printf("%s\n%s\t", penonton[3][0], penonton[3][1]);
+        System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);
+        System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]);
+        System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
+        System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
     }
 }

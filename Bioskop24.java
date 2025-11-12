21efe816bd17e0f592a3eb0f20d34b4fda960a21
Refaliya-Vinandha <revaliya80@gmail.com>
2025-11-12 21:54:57 +0700

    modifikasi 4

--- Bioskop24.java
+++ Bioskop24.java
@@ -24,5 +24,11 @@ public class Bioskop24 {
         for (int i = 0; i < penonton.length; i++) {
             System.out.println("Panjang baris ke-" + (i + 1) + ": " + penonton[i].length);
         }
+
+        System.out.println("Penonton pada baris ke-3: ");
+
+        for (int i = 0; i < penonton[2].length; i++) {
+            System.out.println(penonton[2][i]);
+        }
     }
 }

04f57bd029f05f2d4db5a089f5d68a0dd3376d03
Refaliya-Vinandha <revaliya80@gmail.com>
2025-11-13 09:07:41 +0700

    Percobaan 3

---
+++ Numbers24.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Numbers24 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[][] myNumbers = new int[3][];
+        myNumbers[0] = new int[5];
+        myNumbers[1] = new int[3];
+        myNumbers[2] = new int[1];
+    }
+}

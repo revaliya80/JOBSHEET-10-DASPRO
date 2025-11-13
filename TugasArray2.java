1ed9cc84e27a8eaf351d6d0e4252b26a24d75f25
Refaliya-Vinandha <revaliya80@gmail.com>
2025-11-13 14:47:04 +0700

    Pernyataan a

---
+++ TugasArray2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class TugasArray2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int responden = 10;
+        int pertanyaan = 6;
+        int nilai[][] = new int[responden][pertanyaan];
+
+        for (int i = 0; i < responden; i++) {
+            System.out.println("Responden ke-" + (i + 1));
+
+            for (int j = 0; j < pertanyaan; j++) {
+                System.out.println("Nilai Pertanyaan ke-" + (j + 1) + ": ");
+                nilai[i][j] = sc.nextInt();
+            }
+
+        }
+    }
+}

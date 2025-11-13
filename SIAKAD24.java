533835a9b00a51aece623f1b82c3395b73844d04
Refaliya-Vinandha <revaliya80@gmail.com>
2025-11-13 11:51:51 +0700

    modifikasi 1

--- SIAKAD24.java
+++ SIAKAD24.java
@@ -8,11 +8,14 @@ public class SIAKAD24 {
 
         for (int i = 0; i < nilai.length; i++) {
             System.out.println("Input nilai mahasiswa ke-" + (i + 1));
+            double totalPerSiswa = 0;
 
             for (int j = 0; j < nilai[i].length; j++) {
                 System.out.print("Nilai mata kuliah " + (j + 1) + ": ");
                 nilai[i][j] = sc.nextInt();
+                totalPerSiswa += nilai[i][j];
             }
+            System.out.println("Nilai rata-rata: " + totalPerSiswa / 3);
         }
     }
 }
